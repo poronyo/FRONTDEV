02e20af5e5b8aa2169aeb934fdd75ca48964eb1b
pae <parinyat@syssteel.com>
2025-02-06 07:40:37 +0700

    4guju

--- FontPractice/src/pages/Home.tsx
+++ FontPractice/src/pages/Home.tsx
@@ -1,12 +1,14 @@
+import { FiBattery } from "react-icons/fi";
 function Home() {
   return (
-    <div>
+    <div className="flex flex-col justify-around items-center h-screen">
       <h1>HomePages</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
         fugiat sit architecto nisi expedita rem reiciendis facere quae odio sed!
         Labore quo tenetur eum voluptate ducimus nihil alias quas consectetur?
       </p>
+      <FiBattery className="text-amber-400 size-12" />
     </div>
   );
 }
