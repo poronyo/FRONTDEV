568b858e896c56fb53b90bec564bff01dab6d3fe
pae <parinyat@syssteel.com>
2025-02-07 07:47:12 +0700

    morning update

--- FontPractice/src/pages/Home.tsx
+++ FontPractice/src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { FiCodesandbox, FiTrello } from "react-icons/fi";
 function Home() {
   return (
     <div className="h-full  flex flex-col justify-between items-center bg-cyan-200 dark:bg-gray-500 ">
-      <h1>HomePages </h1>
+      {/* <h1>HomePages </h1> */}
       <div className=" h-full  flex gap-2 p-4 dark:text-amber-100">
         <FiTrello className="h-full rounded-3xl flex basis-1/3 bg-blue-200 dark:bg-gray-900 shadow-2xl " />
         <div className="h-full flex flex-col justify-start rounded-3xl basis-2/3 bg-cyan-200 dark:bg-gray-700 shadow-2xl ">
